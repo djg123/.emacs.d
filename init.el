e6c4dad7ff73892e32dd17570b35e5809c48b564
David J. Gordon <djyale@gmail.com>
2015-09-25 19:22:52 -0400

    Updated

--- init.el
+++ init.el
@@ -25,6 +25,8 @@
 (setq haskell-process-args-cabal-repl
       '("--ghc-option=-ferror-spans" "--with-ghc=ghci-ng"))
 (setq haskell-process-path-ghci "ghci-ng")
+(set-face-background 'shm-current-face "#eee8d5")
+(set-face-background 'shm-quarantine-face "lemonchiffon")
 
 ;;;; Helm Mode (Disable ido mode)
 (require 'helm-config)
@@ -103,7 +105,7 @@
 
 ;;;; Evil Mode
 (require 'evil)
-(evil-mode 1)
+(evil-mode 0)
 (setcdr evil-insert-state-map nil)
 (define-key evil-insert-state-map
             (read-kbd-macro evil-toggle-key) 'evil-normal-state)
@@ -145,6 +147,10 @@
 
 (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
+;;;; Ace-Jump Mode
+(require 'ace-jump-mode)
+
+
 ;;;; YASnippet
 (require 'yasnippet)
 (yas-global-mode 1)
@@ -175,6 +181,9 @@
 ;; (setq jedi:complete-on-dot t)
 (require 'elpy)
 (elpy-enable)
+(setq elpy-rpc-python-command "python")
+(elpy-use-cpython "python")
+(setq python-shell-interpreter "python")
 
 ;;;; Smartparens
 (require 'smartparens)
@@ -194,7 +203,10 @@
  '(custom-safe-themes
    (quote
     ("96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "46fd293ff6e2f6b74a5edf1063c32f2a758ec24a5f63d13b07a20255c074d399" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "72a81c54c97b9e5efcc3ea214382615649ebb539cb4f2fe3a46cd12af72c7607" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" default)))
+ '(inhibit-startup-screen t)
  '(linum-format " %7i ")
+ '(shm-program-name
+   "/home/djg/emacsd-xt/structured-haskell-mode/dist/build/structured-haskell-mode/structured-haskell-mode")
  '(sp-base-key-bindings (quote paredit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
